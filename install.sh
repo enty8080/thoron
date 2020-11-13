47ac9b208a750393bb333395db9e51d1d3d61d78
Ivan Nikolsky <enty8080@gmail.com>
2020-11-13 19:35:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,18 @@ G="\033[1;34m[*] \033[0m"
 S="\033[1;32m[+] \033[0m"
 E="\033[1;31m[-] \033[0m"
 
-if [[ $(id -u) != 0 ]]
-then
-   echo -e ""$E"Permission denied!"
-   exit
+if [[ $(id -u) != 0 ]]; then
+    echo -e ""$E"Permission denied!"
+    exit
 fi
 
 {
-ASESR="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+    CHECK="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 } &> /dev/null
-if [[ "$ASESR" != 0 ]]
-then 
-   echo -e ""$E"No Internet connection!"
-   exit
+
+if [[ "$CHECK" != 0 ]]; then 
+    echo -e ""$E"No Internet connection!"
+    exit
 fi
 
 sleep 0.5
@@ -32,59 +31,56 @@ echo -e ""$G"Installing dependencies..."
 sleep 1
 
 {
-pkg update
-pkg -y install git
-pkg -y install ruby
-apt-get update
-apt-get -y install git
-apt-get -y install ruby
-apk update
-apk add git
-apk add ruby
-pacman -Sy
-pacman -S --noconfirm git
-pacman -S --noconfirm ruby
-zypper refresh
-zypper install -y git
-zypper install -y ruby
-yum -y install git
-yum -y install ruby
-dnf -y install git
-dnf -y install ruby
-eopkg update-repo
-eopkg -y install git
-eopkg -y install ruby
-xbps-install -S
-xbps-install -y git
-xbps-install -y ruby
+    pkg update
+    pkg -y install git
+    pkg -y install ruby
+    apt-get update
+    apt-get -y install git
+    apt-get -y install ruby
+    apk update
+    apk add git
+    apk add ruby
+    pacman -Sy
+    pacman -S --noconfirm git
+    pacman -S --noconfirm ruby
+    zypper refresh
+    zypper install -y git
+    zypper install -y ruby
+    yum -y install git
+    yum -y install ruby
+    dnf -y install git
+    dnf -y install ruby
+    eopkg update-repo
+    eopkg -y install git
+    eopkg -y install ruby
+    xbps-install -S
+    xbps-install -y git
+    xbps-install -y ruby
 } &> /dev/null
 
-if [[ -d ~/thoron ]]
-then
-sleep 0
+if [[ -d ~/thoron ]]; then
+    sleep 0
 else
-cd ~
-{
-git clone https://github.com/EntySec/thoron.git
-} &> /dev/null
+    cd ~
+    {
+        git clone https://github.com/EntySec/thoron.git
+    } &> /dev/null
 fi
 
-if [[ -d ~/thoron ]]
-then
-cd ~/thoron
+if [[ -d ~/thoron ]]; then
+    cd ~/thoron
 else
-echo -e ""$E"Installation failed!"
-exit
+    echo -e ""$E"Installation failed!"
+    exit
 fi
 
 {
-cd bin
-cp thoron /usr/local/bin
-chmod +x /usr/local/bin/thoron
-cp thoron /bin
-chmod +x /bin/thoron
-cp thoron /data/data/com.termux/files/usr/bin
-chmod +x /data/data/com.termux/files/usr/bin/thoron
+    cp bin/thoron /usr/bin
+    chmod +x /usr/bin/thoron
+    cp bin/thoron /usr/local/bin
+    chmod +x /usr/local/bin/thoron
+    cp bin/thoron /data/data/com.termux/files/usr/bin
+    chmod +x /data/data/com.termux/files/usr/bin/thoron
 } &> /dev/null
 
 sleep 1
