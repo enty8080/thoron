e175ce6dfc0fb2582dc8840d94bc73aca80cb1a5
Ivan Nikolsky <enty8080@gmail.com>
2020-11-13 22:35:18 +0300

    Update payloads.rb

--- etc/core/payloads.rb
+++ etc/core/payloads.rb
@@ -1,21 +1,20 @@
 #!/usr/bin/env ruby
 
-i = "\033[1;77m[i] \033[0m"
-e = "\033[1;31m[-] \033[0m"
-p = "\033[1;77m[>] \033[0m"
-g = "\033[1;34m[*] \033[0m"
-s = "\033[1;32m[+] \033[0m"
-h = "\033[1;77m[@] \033[0m"
-r = "\033[1;77m[#] \033[0m"
-
 class Payloads
-    def generate_payload(file, data)
+    def generate_payload(file, data, extension)
+        i = "\033[1;77m[i] \033[0m"
+        e = "\033[1;31m[-] \033[0m"
+        p = "\033[1;77m[>] \033[0m"
+        g = "\033[1;34m[*] \033[0m"
+        s = "\033[1;32m[+] \033[0m"
+        h = "\033[1;77m[@] \033[0m"
+        r = "\033[1;77m[#] \033[0m"
         w = ENV['OLDPWD']
         Dir.chdir(w)
         if File.directory? file
             if File.exists? file
                 if file[-1] == "/"
-                    file = "#{file}payload.cpp"
+                    file = "#{file}payload.#{extension}"
                     sleep(0.5)
                     puts "#{g}Creating payload..."
                     sleep(1)
@@ -26,7 +25,7 @@ class Payloads
                     }
                     puts "#{s}Saved to #{file}!"
                 else
-                    file = "#{file}/payload.cpp"
+                    file = "#{file}/payload.#{extension}"
                     sleep(0.5)
                     puts "#{g}Creating payload..."
                     sleep(1)
