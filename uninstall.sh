b2871b888ac592a2d17031e41ddded936da73ef6
Ivan Nikolsky <enty8080@gmail.com>
2020-11-13 19:39:19 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,15 +6,14 @@ G="\033[1;34m[*] \033[0m"
 S="\033[1;32m[+] \033[0m"
 E="\033[1;31m[-] \033[0m"
 
-if [[ $(id -u) != 0 ]]
-then
-   echo -e ""$E"Permission denied!"
-   exit
+if [[ $(id -u) != 0 ]]; then
+    echo -e ""$E"Permission denied!"
+    exit
 fi
 
 {
-rm -rf ~/thoron
-rm /bin/thoron
-rm /usr/local/bin/thoron
-rm /data/data/com.termux/files/usr/bin/thoron
+    rm -rf ~/thoron
+    rm /usr/bin/thoron
+    rm /usr/local/bin/thoron
+    rm /data/data/com.termux/files/usr/bin/thoron
 } &> /dev/null
